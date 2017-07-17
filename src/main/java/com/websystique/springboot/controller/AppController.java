6b90c0734f937b9ea007df90864f42136cb944d7
Anil Kundu <kundu.anilkumar@gmail.com>
2017-07-17 14:41:43 +0530

    7 title changes

--- src/main/java/com/websystique/springboot/controller/AppController.java
+++ src/main/java/com/websystique/springboot/controller/AppController.java
@@ -10,7 +10,7 @@ public class AppController {
 
 	@RequestMapping("/")
 	String home(ModelMap modal) {
-		modal.addAttribute("title","CRUD 6th Example");
+		modal.addAttribute("title","CRUD 7th Example");
 		return "index";
 	}
 
